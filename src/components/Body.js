e409ad631beeb216ebde2cae5fcde0051af8b517
Priyanshu Singh <singhpriyanshu086@gmail.com>
2024-12-16 20:11:45 +0530

    less go

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,22 @@
 import RestroCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.30971167047457&lng=82.99302231520416&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  };
 
   return (
     <div className="body">
